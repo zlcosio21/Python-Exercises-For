f70d8c444c08c007b642f17b7e39f1c3bc117cbf
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-04-05 00:06:57 -0500

    Ejercicio 4

---
+++ ejercicio4_for.py
@@ -0,0 +1,16 @@
+#Encontrar el menor valor de un conjunto de numeros dados.
+
+
+n = int(input("Ingrese el número de valores a asignar: "))
+
+
+menor_num = 1000000
+
+
+for i in range (1, n + 1):
+  num_asig = int(input(f"Ingrese el valor {i}: "))
+  if menor_num > num_asig:
+    menor_num = num_asig
+
+
+print(f"El menor valor es {menor_num}")
