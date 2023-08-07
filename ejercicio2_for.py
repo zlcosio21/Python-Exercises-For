2a94fa16b2b51292e802d2bbb1adfc1e0cc1a5ee
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-08-06 21:06:48 -0500

    Ejercicios 2 actualizado

--- ejercicio2_for.py
+++ ejercicio2_for.py
@@ -1,36 +1,23 @@
-#Calcular el promedio de edades de hombres, mujeres y de todo un grupo de alumnos.
+#Calcular el promedio de edades de hombres, mujeres, y de todo un grupo de alumnos.
 
+mujeres = int(input("Ingrese cuantas mujeres hay en el grupo: "))
+hombres = int(input("Ingrese cuantas hombres hay en el grupo: "))
 
-grupo = int(input("Ingrese la cantidad de alumnos del grupo: "))
-hombres = int(input("Ingrese la cantidad de hombres del grupo: "))
-mujeres = int(input("Ingrese la cantidad de mujeres del grupo: "))
+sum_mujeres = 0
+sum_hombres = 0
 
+for m in range(1, mujeres + 1):
+  edad_mujeres = int(input(f"Ingrese la edad de la mujer {m}: "))
+  sum_mujeres = sum_mujeres + edad_mujeres
 
-edad_hombres = 0
-edad_mujeres = 0
+for h in range(1, hombres + 1):
+  edad_hombres = int(input(f"Ingrese la edad del hombre {h}: "))
+  sum_hombres = sum_hombres + edad_hombres
 
+promedio_mujeres = sum_mujeres / mujeres
+promedio_hombres = sum_hombres / hombres
+promedio_grupo = (sum_mujeres + sum_hombres) / (mujeres + hombres)
 
-if hombres and mujeres > grupo:
-  print("Por favor ingrese la imformacion correctamente")
-
-
-if hombres and mujeres <= grupo:
-  for i in range(1, hombres + 1):
-    edad_hombre = int(input(f"Ingrese la edad del hombre {i}: "))
-    edad_hombres = edad_hombres + edad_hombre
-
-    
-if hombres and mujeres <= grupo:
-  for e in range(1, mujeres + 1):
-    edad_mujer = int(input(f"Ingrese la edad de la mujer {e}: "))
-    edad_mujeres = edad_mujeres + edad_mujer
-
-
-prom_grupo = (edad_hombres + edad_mujeres) / grupo
-prom_hombres = edad_hombres / hombres
-prom_mujeres = edad_mujeres / mujeres
-
-
-print(f"El promedio del grupo de alumnos es de {prom_grupo}")
-print(f"El promedio de los hombres es de {prom_hombres}")
-print(f"El promedio de las mujeres es de {prom_mujeres}")
+print(f"El promedio de las mujeres es de {round(promedio_mujeres, 2)}")
+print(f"El promedio de los hombres es de {round(promedio_hombres, 2)}")
+print(f"El promedio de todo el grupo de alumnos es {round(promedio_grupo, 2)}")
